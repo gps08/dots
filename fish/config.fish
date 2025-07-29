377f30c532ec462d493290ccd69e85dd91d0c704
gps <gurpreet15140@iiitd.ac.in>
2025-07-28 19:50:35 -0700

    dump current state

--- fish/config.fish
+++ fish/config.fish
@@ -10,18 +10,21 @@ abbr e $EDITOR
 abbr f yazi
 abbr m rmpc
 
+abbr extract_sub 'ffmpeg -i $1 -map 0:s:$2 $1.srt'
+abbr update 'paru'
+
 abbr fc '$EDITOR $CONFIG_HOME/fish/config.fish && source $HOME/.config/fish/config.fish'
 abbr tc '$EDITOR $CONFIG_HOME/tmux/tmux.conf && tmux source-file $HOME/.config/tmux/tmux.conf'
 abbr ec '$EDITOR $CONFIG_HOME/nvim/' 
 abbr yc '$EDITOR $CONFIG_HOME/yazi/'
 abbr kc '$EDITOR $CONFIG_HOME/kitty/kitty.conf'
+abbr se 'sudo -e'
 #abbr du 'sudo du -h --max-depth=1 2>/dev/null'
 
 abbr tn 'tmux new-session -s'
 abbr ta 'tmux attach-session'
 abbr tl 'tmux list-sessions'
 
-abbr gu gitui
 abbr gs 'git status'
 abbr gb 'git branch'
 abbr gl 'git log'
@@ -33,11 +36,6 @@ abbr ga 'git commit --amend -a'
 set fish_greeting
 
 function fish_prompt
-    set_color red
-    switch (hostname)
-        case 'dev-dsk*'; echo -n '[devd] '
-        # case '*'; echo -n '[mac] '
-    end
     set_color $fish_color_cwd
     echo -n (prompt_pwd)
     set_color normal
