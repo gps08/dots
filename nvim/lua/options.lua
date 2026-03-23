eae7679d8902faa849323d27dfd1f5348b6f90cf
Gurpreet <snghzzg@amazon.com>
2026-03-23 10:43:52 -0700

    updates 3/23

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -34,7 +34,7 @@ vim.g.loaded_netrwPlugin = 0
 -- autocommands
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
