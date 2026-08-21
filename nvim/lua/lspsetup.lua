f43e9e051245baa8919f1476fec74f3242c02870
Gurpreet <snghzzg@amazon.com>
2026-08-21 09:33:52 -0700

    nvim keymap cleanup

--- nvim/lua/lspsetup.lua
+++ nvim/lua/lspsetup.lua
@@ -5,3 +5,23 @@ vim.diagnostic.config({
   underline = true,
   severity_sort = true
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local function mapLeader(key, action, desc)
+      vim.keymap.set(
+        { 'n', 'v', 'x' }, '<leader>' .. key, action,
+        { desc = desc, silent = true, buffer = args.buf }
+      )
+    end
+
+    mapLeader('lr', vim.lsp.buf.rename, '[r]ename symbol')
+    mapLeader('lf', vim.lsp.buf.format, '[f]ormat buffer')
+    mapLeader('la', vim.lsp.buf.code_action, 'code [a]ctions')
+    mapLeader('ld', vim.diagnostic.open_float, 'show [d]iagnostics')
+    mapLeader('lq', vim.diagnostic.setqflist, 'Set diagnostic quickfix list')
+    mapLeader('lh', vim.lsp.buf.hover, 'hover documentation')
+    mapLeader('lD', vim.lsp.buf.definition, 'goto [D]efinition')
+    mapLeader('lR', vim.lsp.buf.references, 'goto [R]eferences')
+  end,
+})
